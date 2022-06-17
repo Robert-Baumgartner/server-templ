967a541daf7f5ee08daa9f585067150e37613f6a
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2022-06-17 10:22:06 +0200

    added error handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import dotenv from 'dotenv';
 import path from 'path';
 import testRoute from './routes/test.js';
+import { errorHandler, notFoundHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 
@@ -19,8 +20,9 @@ app.use(express.static(path.join(dirname, '/public')));
 app.use(express.json());
 
 app.use('/test', testRoute);
-
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('server running'));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
