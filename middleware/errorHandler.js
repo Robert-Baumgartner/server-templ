683874561037229c1f7a24b4dbbc291c49d50f55
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2023-02-13 10:06:58 +0100

    removed error handler code

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,16 +1,3 @@
 // eslint-disable-next-line
 import colors from 'colors';
-// eslint-disable-next-line
-const errorHandler = (err, req, res, next) => {
-  console.log(`ERROR ====> ${err.message}`.bgRed.white);
-  console.trace();
-  res.status(500).send('The server has encountered an error');
-};
 
-const notFoundHandler = (req, res, next) => {
-  console.log(`NOT FOUND ====> ${req.originalUrl}`.bgBlue.black);
-  res.status(404).send('The ressource was not found in this server');
-  next();
-};
-
-export { errorHandler, notFoundHandler };
